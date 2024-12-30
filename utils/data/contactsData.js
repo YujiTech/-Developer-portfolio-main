5a300ab46ecf7694135df8fa9cd8abc119314dd9
Renren <johnlawrencemartinez05@gmail.com>
2024-12-30 10:14:28 +0800

    Add files via upload

---
+++ utils/data/contactsData.js
@@ -0,0 +1,11 @@
+export const contactsData = {
+    email: 'johnlawrencemartinez05@gmail.com',
+    phone: '+09388241766',
+    address: 'Cayus, Pilar',  
+    github: 'https://github.com/said7388',
+    facebook: 'https://www.facebook.com/Rence Martinez/',
+    linkedIn: 'https://www.linkedin.com/in/YujiTech-bd/',
+    twitter: 'https://twitter.com/said7388',
+    stackOverflow: 'https://stackoverflow.com/users/16840768/YujiTech',
+    devUsername: "YujiTech"
+}
---
+++ utils/data/personal-data.js
@@ -0,0 +1,17 @@
+export const personalData = {
+  name: "John Martinez",
+  profile: '/profile.png',
+  designation: "Software Developer",
+  description: "My name is John Martinez. I am a professional and enthusiastic programmer in my daily life. I am a quick learner with a self-learning attitude. I love to learn and explore new technologies and am passionate about problem-solving. I love almost all the stacks of web application development and love to make the web more open to the world. My core skill is based on JavaScript and I love to do most of the things using JavaScript. I am available for any kind of job opportunity that suits my skills and interests.",
+  email: 'johnlawrencemartinez05@gmail.com',
+  phone: '+09388241766',
+  address: 'Cayus, Pilar ',
+  github: 'https://github.com/YujiTech',
+  facebook: 'https://www.facebook.com/Rence Martinez/',
+  linkedIn: 'https://www.linkedin.com/in/YujiTech-bd/',
+  twitter: 'https://twitter.com/said7388',
+  stackOverflow: 'https://stackoverflow.com/users/16840768/YujiTech',
+  leetcode: "https://leetcode.com/said3812/",
+  devUsername: "YujiTech",
+  resume: "https://drive.google.com/file/d/1eyutpKFFhJ9X-qpQGKhUNnVRkB5Wer00/view?usp=sharing"
+}
